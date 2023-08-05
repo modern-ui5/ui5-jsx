10f30ac3a3ee24e3e0abdcd601bb0004f1884883
Yichuan Shen <shen.yichuan@gmail.com>
2023-08-05 02:28:31 +0200

    Add JSX test

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,2 @@
 export * from "./ui5-component.js";
+export * from "ui5-typed-model";
--- test/sample.test.tsx
+++ test/sample.test.tsx
@@ -3,7 +3,7 @@
 import "@wdio/globals/types";
 import "./loadUi5.js";
 import { expect } from "@wdio/globals";
-import { Ui5 } from "./dist/main.js";
+import { Ui5, TypedModel } from "./dist/main.js";
 import _VBox from "sap/m/VBox";
 import _Button from "sap/m/Button";
 import _CustomData from "sap/ui/core/CustomData";
@@ -13,14 +13,39 @@ describe("Sample", () => {
   it("should work", async () => {
     expect(5).toEqual(5);
 
+    const model = new TypedModel({
+      items: [{ row: 0 }, { row: 1 }, { row: 2 }],
+    });
     const [VBox, Button, CustomData] = Ui5(_VBox, _Button, _CustomData);
 
     let vbox!: _VBox;
 
-    <VBox ref={(control) => (vbox = control)}>
-      <Button text="Hello World!" />
+    <VBox class="content" ref={(control) => (vbox = control)}>
+      <VBox.items>
+        {model.aggregationBinding(
+          (data) => data.items,
+          (id, model) => (
+            <Button
+              id={id}
+              text={model
+                .binding((_, context) => context.row)
+                .map((row) => `Button ${row}`)}
+              onPress={() => {
+                console.log(
+                  `You clicked row #${model.get((_, context) => context.row)}`
+                );
+              }}
+            />
+          )
+        )}
+      </VBox.items>
     </VBox>;
 
     vbox.placeAt(document.body);
+
+    model.get((data) => data.items).splice(1, 1);
+    model.model.refresh();
+
+    await new Promise((r) => setTimeout(r, 10000000));
   });
 });
